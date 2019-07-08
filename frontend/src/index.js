ac8be7be35c762c853d7a380808dc3a1bfef2c9b
cmoyano <cristianmoyano.mza@gmail.com>
2019-07-08 12:36:43 -0300

    cleaning

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,3 @@
-/* import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import rootReducer from './reducers'
-import App from './components/App'
-
-const store = createStore(rootReducer)
-
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-) */
-
 import React from 'react';
 import {render} from 'react-dom';
 
